10158b0d5d23332eebad591350b365199ff0902c
charos08 <sardorjonmirzayev8@gmail.com>
2026-08-29 16:37:18 +0500

    loyiha strukturasi to'g'rilandi

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Welcome to React + Vite</h1>
+      <p>Company Project</p>
+    </div>
+  )
+}
+
+export default App
